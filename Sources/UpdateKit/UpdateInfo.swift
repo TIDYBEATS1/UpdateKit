4ebad04fa980219f2fcc772735c469f16ac360db
TIDYBEATS1 <samstanwell234@gmail.com>
2025-06-17 13:49:18 +0100

    Remove FirebaseService.swift – moved to host app

--- Sources/UpdateKit/UpdateInfo.swift
+++ Sources/UpdateKit/UpdateInfo.swift
@@ -12,4 +12,11 @@ public struct UpdateInfo {
     public let version: String
     public let downloadURL: URL
     public let patchNotes: String
+
+    // ✅ Add this public initializer:
+    public init(version: String, downloadURL: URL, patchNotes: String) {
+        self.version = version
+        self.downloadURL = downloadURL
+        self.patchNotes = patchNotes
+    }
 }
