c446b5c47db77487396ae061692b4cd6e88e0630
TIDYBEATS1 <samstanwell234@gmail.com>
2025-06-17 14:52:40 +0100

    Remove FirebaseService.swift – moved to host app1

--- Sources/UpdateKit/UpdateInfo.swift
+++ Sources/UpdateKit/UpdateInfo.swift
@@ -8,12 +8,12 @@
 
 import Foundation
 
-public struct UpdateInfo {
+public struct UpdateInfo: Identifiable {
+    public let id = UUID() // 👈 required for .sheet(item:)
     public let version: String
     public let downloadURL: URL
     public let patchNotes: String
 
-    // ✅ Add this public initializer:
     public init(version: String, downloadURL: URL, patchNotes: String) {
         self.version = version
         self.downloadURL = downloadURL
