7d873d68caff1e1e326389adf4556ce297a869f1
TIDYBEATS1 <samstanwell234@gmail.com>
2025-06-17 13:25:16 +0100

    Initial version 2

--- Sources/UpdateKit/UpdateKit.swift
+++ Sources/UpdateKit/UpdateKit.swift
@@ -1,23 +1,8 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
 import Foundation
 
 public enum UpdateKit {
-    
-    /// Checks Firebase Remote Config for a newer version than the one provided.
-    /// - Parameters:
-    ///   - currentVersion: Your app's current version string (e.g., "1.0.0")
-    ///   - completion: Returns an `UpdateInfo` if a newer version is found
-    public static func checkForUpdates(currentVersion: String, completion: @escaping (UpdateInfo?) -> Void) {
-        FirebaseService.fetchUpdateInfo { info in
-            guard let info = info else {
-                completion(nil)
-                return
-            }
-
-            // Compare versions numerically
-            let isNewer = info.version.compare(currentVersion, options: .numeric) == .orderedDescending
-            completion(isNewer ? info : nil)
-        }
+    /// Compares two version strings and returns true if the new one is newer.
+    public static func isNewerVersion(_ latest: String, than current: String) -> Bool {
+        return latest.compare(current, options: .numeric) == .orderedDescending
     }
 }
